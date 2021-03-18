6e3e3cd2de2a199e60b071280f507fb0ff859b3a
Ondřej Charbulák <xcharbul@fi.muni.cz>
2021-03-18 20:02:04 +0100

    Added doc and tests

--- src/common/utility.cpp
+++ src/common/utility.cpp
@@ -1,13 +1,17 @@
 #include <sparse/common/utility.hpp>
 
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 namespace sparse::common {
 
 void print_program_args(int argc_, char* argv_[]) {
+    using json = nlohmann::json;
+    json j = json::array();
     for (int i = 1; i < argc_; ++i) {
-        std::cout << argv_[i] << std::endl;
+        j.push_back(argv_[i]);
     }
+    std::cout << j << std::endl;
 }
 
 } // namespace sparse::common
