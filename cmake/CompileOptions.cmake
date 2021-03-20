897b83c66dcad0ac9a6c1dcc3514f03f56f25c09
Ondřej Charbulák <xcharbul@fi.muni.cz>
2021-03-20 13:32:50 +0100

    Added compiler flags

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -1,2 +1,11 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set(cxx_warnings
+    "-Wall -Wextra -Wpedantic -Wdouble-promotion -Wnull-dereference -Wimplicit-fallthrough \
+    -Wswitch-default -Wuninitialized -Wsuggest-attribute=const -Wsuggest-attribute=noreturn \
+    -Wfloat-equal -Wshadow -Wconversion"
+)
+
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_FORTIFY_SOURCE=2 -fstack-clash-protection -fstack-protector-strong ${cxx_warnings}")
