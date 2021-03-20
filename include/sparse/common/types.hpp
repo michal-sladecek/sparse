f7e7f9f124b95512748ad355f93f01f752bb54ce
mdugacek <matus.dugacek@protonmail.com>
2021-03-20 22:24:45 +0100

    add structs representing parts of the parsed certificate

---
+++ include/sparse/common/types.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+namespace sparse::common
+{
+/**
+ * @brief Parsed title of the certificate
+ */
+struct title
+{
+    using data_t = std::string;
+
+    data_t data;
+};
+
+/**
+ * @brief Parsed versions of the certificate
+ */
+struct versions
+{
+    using data_t = std::unordered_map<std::string, std::vector<std::string>>;
+
+    data_t data;
+};
+
+/**
+ * @brief Parsed table of content of the certificate
+ */
+struct table_of_content
+{
+    using data_t = std::vector<std::tuple<std::string, std::string, int>>;
+
+    data_t data;
+};
+
+/**
+ * @brief Parsed revisions of the certificate
+ */
+struct revisions
+{
+    using data_t = std::vector<std::unordered_map<std::string, std::string>>;
+
+    data_t data;
+};
+
+/**
+ * @brief Parsed bibliography of the certificate
+ */
+struct bibliography
+{
+    using data_t = std::unordered_map<std::string, std::string>;
+
+    data_t data;
+};
+
+/**
+ * @brief Parsed section other of the certificate
+ */
+struct other
+{
+    using data_t = std::unordered_map<std::string, std::string>;
+
+    data_t data;
+};
+} // namespace sparse::common
