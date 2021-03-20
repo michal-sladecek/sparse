24f5960722a876ad232aca3ce82f4307ca281011
Ondřej Charbulák <xcharbul@fi.muni.cz>
2021-03-20 12:26:54 +0100

    Dependencies.cmake now use FetchContent

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,6 +1,23 @@
-set(ext_dir ${CMAKE_SOURCE_DIR}/external)
+Include(FetchContent)
 
-add_subdirectory("${ext_dir}/Catch2")
+# Add Catch2
+FetchContent_Declare(
+    Catch2
+    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+    GIT_TAG v2.13.4
+)
 
-set(JSON_BuildTests OFF CACHE INTERNAL "")
-add_subdirectory("${ext_dir}/nlohmann_json")
+FetchContent_MakeAvailable(Catch2)
+
+# Add nlohmann json
+FetchContent_Declare(
+    json
+    GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent
+    GIT_TAG v3.9.1
+)
+
+FetchContent_GetProperties(json)
+if(NOT json_POPULATED)
+  FetchContent_Populate(json)
+  add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
