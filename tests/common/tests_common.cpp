6a62bc7bb37fc42ae2081e4d0bd966f05cffb882
Michal Sladecek <469434@mail.muni.cz>
2021-03-24 16:56:30 +0100

    Tests for loading file

--- tests/common/tests_common.cpp
+++ tests/common/tests_common.cpp
@@ -1,6 +1,18 @@
 #include <catch2/catch.hpp>
+#include <sparse/common/utility.hpp>
+
 
 TEST_CASE("test")
 {
     REQUIRE(true);
+}
+
+
+TEST_CASE("load_file_into_string")
+{
+    std::string whole_text = sparse::common::load_file_into_string("../1107a_pdf.txt");
+    REQUIRE(whole_text.length() > 0);
+
+    std::string file_does_not_exist = sparse::common::load_file_into_string("../1107a_pf.txt");
+    REQUIRE(file_does_not_exist.length() == 0);
 }
