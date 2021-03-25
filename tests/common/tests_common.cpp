dc5d854a231f4d16a27b11492c7d76db56a132dd
Michal Sladecek <469434@mail.muni.cz>
2021-03-25 17:50:11 +0100

    Added some error handling to file loading function

--- tests/common/tests_common.cpp
+++ tests/common/tests_common.cpp
@@ -3,9 +3,8 @@
 
 TEST_CASE("load_file_into_string")
 {
-    std::string whole_text = sparse::common::load_file_into_string("../1107a_pdf.txt");
+    std::string whole_text = sparse::common::load_file_into_string("../parsers/1107a_pdf.txt");
     REQUIRE(whole_text.length() > 0);
 
-    std::string file_does_not_exist = sparse::common::load_file_into_string("../1107a_pf.txt");
-    REQUIRE(file_does_not_exist.length() == 0);
+    REQUIRE_THROWS_WITH(sparse::common::load_file_into_string("nonexisting.txt"),Catch::Contains("File does not exist"));
 }
