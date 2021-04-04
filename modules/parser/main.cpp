bf9a47cf4098443624d5530df53256dc04e1693b
Ondřej Charbulák <xcharbul@fi.muni.cz>
2021-04-04 11:23:45 +0200

    Added missing return statemens

--- modules/parser/main.cpp
+++ modules/parser/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[])
     if (argc < 2)
     {
         std::cerr << "Missing filename argument" << std::endl;
+        return EXIT_FAILURE;
     }
     auto whole_file = sparse::common::load_file_into_string(argv[1]);
 
@@ -23,4 +24,5 @@ int main(int argc, char* argv[])
     }
 
     std::cout << std::setw(4) << output;
+    return EXIT_SUCCESS;
 }
