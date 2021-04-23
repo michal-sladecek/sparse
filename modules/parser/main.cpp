7d275cf3f582ff2633a84a5e314102a3d9a2b3b2
Ondřej Charbulák <xcharbul@fi.muni.cz>
2021-04-23 23:44:34 +0200

    Added function for parsing program options

--- modules/parser/main.cpp
+++ modules/parser/main.cpp
@@ -4,15 +4,92 @@
 #include <sparse/common/utility.hpp>
 #include <sparse/parsers/parsers.hpp>
 #include <string>
+#include <string_view>
+
+namespace fs = std::filesystem;
+
+struct options_t
+{
+    bool parse_title{};
+    bool parse_toc{};
+    bool parse_versions{};
+    bool parse_revisions{};
+    bool parse_bib{};
+
+    bool use_cout{};
+    std::vector<fs::path> items_to_parse;
+};
+
+void print_help_message()
+{
+    std::cout << "SPARSE TODO: Description\n";
+    std::cout << "\nSupported program arguments:\n";
+}
+
+options_t parse_program_options(int argc_, char* argv_[])
+{
+    options_t o{};
+    bool some_set{};
+    bool print_help{};
+    const auto arg_map = [&]() {
+        std::map<std::string_view, std::pair<std::function<void()>, std::string_view>> m;
+        // clang-format off
+        m.insert({"--title",        {[&]() { some_set = o.parse_title = true; },     "Parse document Title."}});
+        m.insert({"--toc",          {[&]() { some_set = o.parse_toc = true; },       "Parse Table of Content."}});
+        m.insert({"--versions",     {[&]() { some_set = o.parse_versions = true; },  "Parse Versions."}});
+        m.insert({"--revisions",    {[&]() { some_set = o.parse_revisions = true; }, "Parse Revisions."}});
+        m.insert({"--bibliography", {[&]() { some_set = o.parse_bib = true; },       "Parse Bibliography."}});
+        m.insert({"--out",          {[&]() { o.use_cout = true; },                   "Print result json to standard output."}});
+        m.insert({"--help",         {[&]() { print_help = true; },                   "Show program options and exit."}});
+        m.insert({"-h",             {[&]() { print_help = true; },                   ""}});
+        // clang-format on
+        return m;
+    }();
+
+    for (int i = 1; i < argc_; ++i)
+    {
+        if (const std::string_view arg{argv_[i]}; arg_map.contains(arg))
+        {
+            std::invoke(arg_map.at(arg).first);
+        }
+        else
+        {
+            o.items_to_parse.emplace_back(arg);
+        }
+    }
+
+    if (print_help || argc_ <= 1)
+    {
+        print_help_message();
+        for (const auto& [key, val] : arg_map)
+        {
+            if (std::string_view msg{val.second}; !msg.empty())
+            {
+                std::cout << "  " << std::setw(18) << std::left << key << msg << std::endl;
+            }
+        }
+        exit(EXIT_SUCCESS);
+    }
+    if (!some_set)
+    {
+        o.parse_bib = o.parse_toc = o.parse_title = o.parse_versions = o.parse_revisions = true;
+    }
+    for (const auto& path : o.items_to_parse)
+    {
+        if (!fs::is_directory(path) && !fs::is_regular_file(path))
+        {
+            std::cerr << "Error: Parameter " << path << " is neither file nor directory.";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return o;
+}
 
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        std::cerr << "Missing filename argument" << std::endl;
-        return EXIT_FAILURE;
-    }
+    const auto options    = parse_program_options(argc, argv);
     const auto whole_file = sparse::common::load_file_into_string(argv[1]);
 
     nlohmann::json output;
